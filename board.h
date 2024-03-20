4b64f54cafa256d3a6ac45777605ace6bc44fd09
gregghy <gregoriosilves@gmail.com>
2024-03-20 11:25:18 +0100

    generate_board done

--- board.h
+++ board.h
@@ -3,7 +3,7 @@ typedef struct board {
   int cols;
   int car[4][4];
   int seen[4][4];
-} * board_t;
+} board_t;
 
 void printBoard(board_t board);
 board_t generateBoard(int r, int c);
--- generate_board.c
+++ generate_board.c
@@ -6,27 +6,34 @@
 void printBoard (board_t b) {
   int i, j;
 
-  for (i = 0; i<b -> rows; i++) {
-    for (j = 0; j<b -> cols; j++) {
-      printf("%d | \n", b -> car[i][j]);
+  for (i = 0; i<b.rows; i++) {
+    for (j = 0; j<b.cols; j++) {
+      printf("rows %d, col %d, val %d, seen %d | \n", i, j, b.car[i][j], b.seen[i][j]);
       printf("- \n");
     }
   }
 }
 
+int randomChar() {
+  int randomInt = rand();
+  randomInt = randomInt % 26;
+  //convert random integer to ASCII uppercase letter
+  int randomCharacter = randomInt + 65;
+  return randomCharacter;
+}
+
 board_t generateBoard (int r, int c){
   int i, j;
   board_t b;
-  b = malloc(sizeof(board_t));
-  b->rows = r;
-  b->cols = c;
+  b.rows = r;
+  b.cols = c;
 
   for (i=0; i<r; i++) {
     for (j=0; j<c; j++) {
       printf("i = %d ", i);
       printf("j = %d\n", j);
-      b -> car[i][j] = 20;
-      b -> seen[i][j] = 0;
+      b.car[i][j] = randomChar();
+      b.seen[i][j] = 0;
     }
   }
   return b;
